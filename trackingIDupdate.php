a10895922686e602248c828dda3213b156ed4e0a
s0544780 <s0544780@htw-berlin.de>
2016-10-11 18:34:01 +0200

    TrackingIDs 2 CSV

--- trackingIDupdate.php
+++ trackingIDupdate.php
@@ -9,6 +9,14 @@
 
 require_once("inc/config.inc.php");
 require_once("inc/functions.inc.php");
+session_start();
+$user = check_user();
+$t=time();
+
+$upload_folder = 'upload/'.($user['username']) .'/'; //Das Upload-Verzeichnis
+	if (!file_exists($upload_folder)) {
+		mkdir($upload_folder, 0777, true);
+	}
 
 
 if(isset($_POST['trackingID']) and isset($_POST["BestellNr"])){
@@ -18,8 +26,22 @@ if(isset($_POST['trackingID']) and isset($_POST["BestellNr"])){
     $statement = $pdo->prepare("UPDATE tbestellung SET cTracking='{$trackingID}' WHERE cBestellNr='{$bestellNr}'");
     var_dump($statement);
     $result = $statement->execute();
-    $user = $statement->fetch();
-    echo "TrackingID erfolgreich aktualisiert!<br><br><a href=\"javascript:history.go(-1)\">zurück zur vorherigen Seite</a>";
+    #$user = $statement->fetch();
+    
+    
+    $dz=fopen($upload_folder.date("Y_m_d_",$t)."neueTrackingIDS.csv","a");
+		 if(!$dz)
+			 {
+				 $note = "Datei konnte nicht zum Schreiben geöffnet werden.";
+				 exit;
+			 }
+
+ fputs($dz,$trackingID.";".$bestellNr.";".date("Y_m_d",$t)."\n");
+
+fclose($dz);	 
+echo '<script>alert("TrackingID erfolgreich aktualisiert.")</script>';
+
+echo "<script>window.location = 'http://localhost:8888/Webservice/verkaeufe.php'</script>";
     } else {
        echo "nicht aktualisiert<br><br>";
     }
